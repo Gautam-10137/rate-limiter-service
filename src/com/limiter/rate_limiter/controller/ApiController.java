3fa3d7ed5b11b53a185343f2e112a35544b69a43
Gautam-10137 <pahwagautam786@gmail.com>
2025-09-18 18:20:37 +0530

    controller added

---
+++ src/com/limiter/rate_limiter/controller/ApiController.java
@@ -0,0 +1,28 @@
+package com.limiter.rate_limiter.controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.limiter.rate_limiter.annotation.RateLimit;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+
+	
+	@GetMapping("/info")
+	@RateLimit
+	public ResponseEntity<String>  getInfo() {
+		return ResponseEntity.ok("hello_gautam");
+	}
+	
+	
+	@GetMapping("/data")
+	@RateLimit(capacity=5,refillTokensPerSecond=1)
+	public ResponseEntity<String>  getData() {
+		return ResponseEntity.ok("hello_gautam");
+	}
+	
+}
