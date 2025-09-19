c7a1cbe68a09c5abd2db14d26c31e24d61f4b2c7
Gautam-10137 <pahwagautam786@gmail.com>
2025-09-19 08:43:37 +0530

    exception handler updated

--- src/com/limiter/rate_limiter/exception/GlobalExceptionHandler.java
+++ src/com/limiter/rate_limiter/exception/GlobalExceptionHandler.java
@@ -1,16 +1,31 @@
 package com.limiter.rate_limiter.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.limiter.rate_limiter.service.RateLimiterService;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
+	
+	private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
 	@ExceptionHandler(TooManyRequestsException.class)
-	public ResponseEntity<String> handleTooManyRequests(TooManyRequestsException exc) {
-		return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).body(exc.getMessage());
+	public ResponseEntity<String> handleTooManyRequests(TooManyRequestsException ex) {
+		log.error("RuntimeException occurred: {}", ex.getMessage());
+		return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).body(ex.getMessage());
 	}
+	
+	
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        log.error("Unexpected exception: {}", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal server error");
+    }
 
 }
