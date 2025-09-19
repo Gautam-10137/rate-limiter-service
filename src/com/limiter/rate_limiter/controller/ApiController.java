42925d752b8c741b74b036d29c0fc1a42d02c3e7
Gautam-10137 <pahwagautam786@gmail.com>
2025-09-19 08:42:20 +0530

    controller updated

--- src/com/limiter/rate_limiter/controller/ApiController.java
+++ src/com/limiter/rate_limiter/controller/ApiController.java
@@ -13,14 +13,14 @@ public class ApiController {
 
 	
 	@GetMapping("/info")
-	@RateLimit
+	@RateLimit(capacity = 5, refillTokensPerSecond = 1)  // defining custom values for property 
 	public ResponseEntity<String>  getInfo() {
 		return ResponseEntity.ok("hello_gautam");
 	}
 	
 	
 	@GetMapping("/data")
-	@RateLimit(capacity=5,refillTokensPerSecond=1)
+	@RateLimit(capacity = 3, refillTokensPerSecond = 1)
 	public ResponseEntity<String>  getData() {
 		return ResponseEntity.ok("hello_gautam");
 	}
